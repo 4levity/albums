da69d6e2f2e58b201cbab24d2aee97306881879e
4levity <3319205+4levity@users.noreply.github.com>
2026-02-11 14:00:12 -0800

    fixer for invalid image files (not embedded yet)

--- tests/checks/test_check_invalid_image.py
+++ tests/checks/test_check_invalid_image.py
@@ -1,3 +1,6 @@
+from pathlib import Path
+from unittest.mock import call
+
 from albums.app import Context
 from albums.checks.check_invalid_image import CheckInvalidImage
 from albums.types import Album, Picture, PictureType, Stream, Track
@@ -10,15 +13,25 @@ class TestCheckCheckInvalidImage:
         assert not CheckInvalidImage(Context()).check(album)
 
     def test_error_image_in_track(self):
-        pic = Picture(PictureType.COVER_FRONT, "image/png", 400, 400, 0, b"", {"error": "couldn't load image"})
+        pic = Picture(PictureType.COVER_FRONT, "image/png", 400, 400, 0, b"", {"error": "test load failed"})
         album = Album("", [Track("1.flac", {}, 0, 0, Stream(1.5, 0, 0, "FLAC"), [pic])])
         result = CheckInvalidImage(Context()).check(album)
         assert result is not None
-        assert "invalid images (couldn't load image): 1.flac#0" in result.message
+        assert "image load errors: test load failed" in result.message
+        assert album.tracks[0].filename in result.message
 
-    def test_error_image_in_file(self):
-        pic = Picture(PictureType.COVER_FRONT, "image/png", 400, 400, 0, b"", {"error": "couldn't load image"})
+    def test_error_image_in_file(self, mocker):
+        pic = Picture(PictureType.COVER_FRONT, "image/png", 400, 400, 0, b"", {"error": "test load failed"})
         album = Album("", [Track("1.flac", {}, 0, 0, Stream(1.5, 0, 0, "FLAC"))], [], [], {"cover.jpg": pic})
         result = CheckInvalidImage(Context()).check(album)
         assert result is not None
-        assert "invalid images (couldn't load image): cover.jpg" in result.message
+        assert "image load errors: test load failed" in result.message
+        assert result.fixer
+        assert result.fixer.option_automatic_index is None
+        assert len(result.fixer.options) == 1
+        assert "delete the invalid image files" in result.fixer.options[0]
+
+        mock_unlink = mocker.patch("albums.checks.check_invalid_image.unlink")
+        fix_result = result.fixer.fix(result.fixer.options[0])
+        assert fix_result
+        assert mock_unlink.call_args_list == [call(Path(album.path) / "cover.jpg")]
