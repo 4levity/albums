7fa8d5ab62838ba7ff411fa2bfe2048d2020f9cd
4levity <3319205+4levity@users.noreply.github.com>
2026-04-01 17:49:43 -0700

    destination configurator test

--- tests/test_configurator.py
+++ tests/test_configurator.py
@@ -1,4 +1,7 @@
-from albums.app import Context
+import os
+import platform
+
+from albums.app import Context, Path
 from albums.database import connection, db_config
 from albums.interactive.configurator import interactive_config
 
@@ -39,4 +42,43 @@ class TestConfigurator:
         finally:
             ctx.db.dispose()
 
-    # TODO test settings, check configs
+    def test_config_new_sync_dest(self, mocker):
+        ctx = Context()
+        ctx.db = connection.open(connection.MEMORY)
+        abs_path = "C:\\Music" if platform.system() == "Windows" else "/usr/share/music"
+        try:
+            mock_main_menu_choice = mocker.patch("albums.interactive.configurator.choice")
+            mock_main_menu_choice.side_effect = ["destinations", "exit"]
+            mock_destinations_choice = mocker.patch("albums.interactive.setup_destination.choice")
+            mock_destinations_choice.side_effect = [
+                "new",
+                "",
+                "relpath_template_artist",
+                "relpath_template_compilation",
+                "save",
+                "back",
+            ]  # new destination, new collection, save destination, back to main menu
+            mock_prompt = mocker.patch("albums.interactive.setup_destination.prompt")
+            template1 = f"$artist{os.sep}$album"
+            template2 = f"Various{os.sep}$album"
+            mock_prompt.side_effect = [
+                abs_path,
+                "test",
+                template1,
+                template2,
+            ]  # destination path, collection name, relpath_template_artist, relpath_template_compilation
+
+            interactive_config(ctx)
+
+            assert mock_main_menu_choice.call_count == 2
+            assert mock_destinations_choice.call_count == 6
+            assert mock_prompt.call_count == 4
+            config = db_config.load(ctx.db)
+            assert len(config.sync_destinations) == 1
+            dest = config.sync_destinations[0]
+            assert dest.path_root == Path(abs_path)
+            assert dest.collection == "test"
+            assert dest.relpath_template_artist.template == template1
+            assert dest.relpath_template_compilation.template == template2
+        finally:
+            ctx.db.dispose()
