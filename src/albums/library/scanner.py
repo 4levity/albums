829771e17176d7c8cfcfcadf274846eda7d518b4
4levity <3319205+4levity@users.noreply.github.com>
2026-08-10 00:50:26 -0700

    simplify _scan_track

--- src/albums/library/scanner.py
+++ src/albums/library/scanner.py
@@ -223,12 +223,9 @@ def _scan_track(tagger: AlbumTagger, filename: str, stat: MiniStat, target_scan:
 
         if target_scan is not None and not target_scan.tags and isinstance(target_scan.source, Track):
             tags = [TagV(tag=t.tag, value=t.value) for t in target_scan.source.tags]
-        else:
-            tags = [TagV(tag=tag, value=value) for tag, values in file.get_tags() for value in values]
-
-        if target_scan is not None and not target_scan.tags and isinstance(target_scan.source, Track):
             legacy_tags = list(target_scan.source.legacy_tags)
         else:
+            tags = [TagV(tag=tag, value=value) for tag, values in file.get_tags() for value in values]
             legacy_tags = [tag_name for (tag_name, _) in file.get_legacy_tags()]
 
         if target_scan is not None and not target_scan.images and isinstance(target_scan.source, Track):
